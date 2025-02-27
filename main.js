39e5f2f70eeede4cfe619b04e27c72e1f4467dc5
Nattpatsu <nattpat.su@gmail.com>
2025-02-27 17:26:02 +0700

    update time countdown 30s

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const countNumber = document.getElementById("number");
+const changeTime = document.getElementById("time");
+
 let count = 0;
+let second = 30;
+let interval = null;
 
 const test = () => {
   alert("Waiting for upgrade");
@@ -8,10 +12,38 @@ const test = () => {
 const changeNumber = () => {
   count++;
   countNumber.innerHTML = count;
+  timer();
+  startTime();
   //console.log(count);
 };
 
 const resetNumber = () => {
   count = 0;
   countNumber.innerHTML = count;
+};
+
+//Timer set up
+
+const setTime = () => {
+  //const minutes = Math.floor(second / 60);
+  const seconds = second % 60;
+  changeTime.innerHTML = `${seconds} s`;
+  console.log("settime");
+  seconds === 0 ? stopTime() : console.log(seconds);
+};
+
+const timer = () => {
+  second--;
+  setTime();
+  //console.log("timer");
+};
+
+const startTime = () => {
+  //clearInterval(interval);
+  interval = setInterval(timer, 1000);
+};
+
+const stopTime = () => {
+  clearInterval(interval);
+  second = 30;
 };
